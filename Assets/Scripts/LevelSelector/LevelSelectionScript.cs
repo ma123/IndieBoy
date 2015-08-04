8168a7b97b31846fd3dcef1b94ac7d78ebd52d73
msukenik <m45sukenik@gmail.com>
2015-08-05 00:30:19 +0200

    cc
    nove sceny

--- Assets/Scripts/LevelSelector/LevelSelectionScript.cs
+++ Assets/Scripts/LevelSelector/LevelSelectionScript.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI; 
 using System.Collections;
 
 public class LevelSelectionScript : MonoBehaviour {
+	private int openedLevel = 1;	
+	public GameObject gameObject;
 
-	public void OnClickedLevel() {
-		Application.LoadLevel ("Level1");
+	void Start() {
+		gameObject.GetComponent<Button>().interactable = false;
+	}
+
+	public void OnClickedLevel(int numberOfLevel) {
+		Application.LoadLevel ("Level" + numberOfLevel);
 	}
 }
